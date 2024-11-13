6662fc088c6402d9ce9e536eaa79a2382f70a2a8
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-11-13 11:12:22 -0600

    sefvesve

--- js/manifest.js
+++ js/manifest.js
@@ -1,82 +1,84 @@
-{
-    "manifest": "arweave/paths",
-    "version": "0.1.0",
-    "index": {
-      "path": "index.html"
+const manifest = {
+  "manifest": "arweave/paths",
+  "version": "0.1.0",
+  "index": {
+    "path": "index.html"
+  },
+  "paths": {
+    "index.html": {
+      "id": "oc24ulDYknaP32SDVnwB1jdUPGqhxUZWpuZHXnjaDlg"
     },
-    "paths": {
-      "index.html": {
-        "id": "oc24ulDYknaP32SDVnwB1jdUPGqhxUZWpuZHXnjaDlg"
-      },
-      "main.css": {
-        "id": "bGdaqZf6EQnZLGKtiFj4iKCb1tO4xapLDteAxel4Lw4"
-      },
-      "crudeboycards/1.png": {
-        "id": "LD8thD1KNTotlSvHOMJVPybiCl807fGlJMWtu5KVuN0"
-      },
-      "crudeboycards/2.png": {
-        "id": "FRDNNqAmrwHBWr-UCZQQG80hG7OpLUntLveMt0AB7bE"
-      },
-      "crudeboycards/4.png": {
-        "id": "2IGi81Uke6P65ZkG76oGRbQ0AkPLsmLVxyojCytvyTY"
-      },
-      "crudeboycards/5.png": {
-        "id": "2EBjRnJCWup4hdd0Xjbh44NUF5aBsvChV-kg2F1beEw"
-      },
-      "crudeboycards/6.png": {
-        "id": "8zjlTaUIqeACbBrh6FrY-Q2xIXsw5b5rDahvxqH36Ys"
-      },
-      "crudeboycards/7.png": {
-        "id": "znxrxVhRFK1vfAvMwTZxWp4-BL8zBD8SQd_GgtkvEfc"
-      },
-      "crudeboycards/8.png": {
-        "id": "-ebk0BQ_T8vKsAhj6nnamH4A7f9iX_cC76g_vS-xYiw"
-      },
-      "crudeboycards/9.png": {
-        "id": "E7X3tRPinAmWIZLLoLdxyHTzJY0X2Wi4D3PuVQnT4AM"
-      },
-      "crudeboycards/10.png": {
-        "id": "GMOiQptD0gTzhHkchcDvL8JO6oQBpmiSLdNLL1ujiTc"
-      },
-      "crudeboycards/11.png": {
-        "id": "dRV_HK5fR-jmXEqXvSj6UB2z-eSI7xUvJbQWNjP_DaY"
-      },
-      "crudeboycards/12.png": {
-        "id": "eofIhaehyRucDrWDQpVpoLFYEnQSWBnV6zJ_rwDMTHA"
-      },
-      "crudeboycards/13.png": {
-        "id": "5uAtB9giMOEM2m2UfxIdmKPYSy01hD-Z0UI_quY45z8"
-      },
-      "crudeboycards/14.png": {
-        "id": "0AtGGPZKgaVaPga65VltAlUY-40q6We_EpLx9QUzBhg"
-      },
-      "crudeboycards/15.png": {
-        "id": "-Sp_8fLvO7WewgOGQk2k-1Nupr0nB3MLnJnucTHXqfA"
-      },
-      "crudeboycards/16.png": {
-        "id": "LhDdy2vDuZG2_Ae0ejWnzcVk64qQOeu5pu97iAgB6GU"
-      },
-      "crudeboycards/17.png": {
-        "id": "MxGIRaYWgyr2xJnJiwGh4s9It1Ejid4GWvyyhmGbw30"
-      },
-      "crudeboycards/18.png": {
-        "id": "uU3ZDL5UiAcrbkFxMFhu6FoakmT6YwKjwGbEgJKP7BM"
-      },
-      "crudeboycards/19.png": {
-        "id": "RhVg5LCykU-fK2Lq8ErnRq0o1k8eCaF9nYlLvQZFYiI"
-      },
-      "crudeboycards/20.png": {
-        "id": "whK0Nhy72O-CCNRXgmUlwge1SYMHyhrV_6FAO6uvkaU"
-      },
-      "crudeboycards/21.png": {
-        "id": "V0XXw42td3rmbJ4i8ijMfK0Ho_-meeqC_HCJsTxUIuk"
-      },
-      "crudeboycards/22.png": {
-        "id": "jWDJ2L88pDjfG_FwVRVBV--RJU2Kk_K7lld-Yvrw36M"
-      },
-      "js/crudeboys_image.json": {
-        "id": "hOi37GKaCGipB-8mD-psqDCkIAUGv1BC9AH_xgt8hP4"
-      }
+    "main.css": {
+      "id": "bGdaqZf6EQnZLGKtiFj4iKCb1tO4xapLDteAxel4Lw4"
+    },
+    "crudeboycards/1.png": {
+      "id": "LD8thD1KNTotlSvHOMJVPybiCl807fGlJMWtu5KVuN0"
+    },
+    "crudeboycards/2.png": {
+      "id": "FRDNNqAmrwHBWr-UCZQQG80hG7OpLUntLveMt0AB7bE"
+    },
+    "crudeboycards/4.png": {
+      "id": "2IGi81Uke6P65ZkG76oGRbQ0AkPLsmLVxyojCytvyTY"
+    },
+    "crudeboycards/5.png": {
+      "id": "2EBjRnJCWup4hdd0Xjbh44NUF5aBsvChV-kg2F1beEw"
+    },
+    "crudeboycards/6.png": {
+      "id": "8zjlTaUIqeACbBrh6FrY-Q2xIXsw5b5rDahvxqH36Ys"
+    },
+    "crudeboycards/7.png": {
+      "id": "znxrxVhRFK1vfAvMwTZxWp4-BL8zBD8SQd_GgtkvEfc"
+    },
+    "crudeboycards/8.png": {
+      "id": "-ebk0BQ_T8vKsAhj6nnamH4A7f9iX_cC76g_vS-xYiw"
+    },
+    "crudeboycards/9.png": {
+      "id": "E7X3tRPinAmWIZLLoLdxyHTzJY0X2Wi4D3PuVQnT4AM"
+    },
+    "crudeboycards/10.png": {
+      "id": "GMOiQptD0gTzhHkchcDvL8JO6oQBpmiSLdNLL1ujiTc"
+    },
+    "crudeboycards/11.png": {
+      "id": "dRV_HK5fR-jmXEqXvSj6UB2z-eSI7xUvJbQWNjP_DaY"
+    },
+    "crudeboycards/12.png": {
+      "id": "eofIhaehyRucDrWDQpVpoLFYEnQSWBnV6zJ_rwDMTHA"
+    },
+    "crudeboycards/13.png": {
+      "id": "5uAtB9giMOEM2m2UfxIdmKPYSy01hD-Z0UI_quY45z8"
+    },
+    "crudeboycards/14.png": {
+      "id": "0AtGGPZKgaVaPga65VltAlUY-40q6We_EpLx9QUzBhg"
+    },
+    "crudeboycards/15.png": {
+      "id": "-Sp_8fLvO7WewgOGQk2k-1Nupr0nB3MLnJnucTHXqfA"
+    },
+    "crudeboycards/16.png": {
+      "id": "LhDdy2vDuZG2_Ae0ejWnzcVk64qQOeu5pu97iAgB6GU"
+    },
+    "crudeboycards/17.png": {
+      "id": "MxGIRaYWgyr2xJnJiwGh4s9It1Ejid4GWvyyhmGbw30"
+    },
+    "crudeboycards/18.png": {
+      "id": "uU3ZDL5UiAcrbkFxMFhu6FoakmT6YwKjwGbEgJKP7BM"
+    },
+    "crudeboycards/19.png": {
+      "id": "RhVg5LCykU-fK2Lq8ErnRq0o1k8eCaF9nYlLvQZFYiI"
+    },
+    "crudeboycards/20.png": {
+      "id": "whK0Nhy72O-CCNRXgmUlwge1SYMHyhrV_6FAO6uvkaU"
+    },
+    "crudeboycards/21.png": {
+      "id": "V0XXw42td3rmbJ4i8ijMfK0Ho_-meeqC_HCJsTxUIuk"
+    },
+    "crudeboycards/22.png": {
+      "id": "jWDJ2L88pDjfG_FwVRVBV--RJU2Kk_K7lld-Yvrw36M"
+    },
+    "js/crudeboys_image.json": {
+      "id": "hOi37GKaCGipB-8mD-psqDCkIAUGv1BC9AH_xgt8hP4"
     }
   }
+};
+
+export default manifest;
   
