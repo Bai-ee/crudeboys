9c0c3977c6a2551c895706d403520e43d9ec9cd5
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-11-13 12:49:59 -0600

    Goldies4

--- js/manifest.js
+++ js/manifest.js
@@ -11,68 +11,59 @@ const manifest = {
     "main.css": {
       "id": "bGdaqZf6EQnZLGKtiFj4iKCb1tO4xapLDteAxel4Lw4"
     },
-    "https://bafybeidm3sremjulcdqefulerybnjqtzcf2o3vvyu5ayg35lbthmhxs5hi.ipfs.dweb.link/1.png": {
-      "id": "LD8thD1KNTotlSvHOMJVPybiCl807fGlJMWtu5KVuN0"
-    },
-    "https://bafybeidm3sremjulcdqefulerybnjqtzcf2o3vvyu5ayg35lbthmhxs5hi.ipfs.dweb.link/2.png": {
-      "id": "FRDNNqAmrwHBWr-UCZQQG80hG7OpLUntLveMt0AB7bE"
-    },
-    "https://bafybeidm3sremjulcdqefulerybnjqtzcf2o3vvyu5ayg35lbthmhxs5hi.ipfs.dweb.link/4.png": {
+    "images/four.png": {
       "id": "2IGi81Uke6P65ZkG76oGRbQ0AkPLsmLVxyojCytvyTY"
     },
-    "https://bafybeidm3sremjulcdqefulerybnjqtzcf2o3vvyu5ayg35lbthmhxs5hi.ipfs.dweb.link/5.png": {
+    "images/five.png": {
       "id": "2EBjRnJCWup4hdd0Xjbh44NUF5aBsvChV-kg2F1beEw"
     },
-    "https://bafybeidm3sremjulcdqefulerybnjqtzcf2o3vvyu5ayg35lbthmhxs5hi.ipfs.dweb.link/6.png": {
+    "images/six.png": {
       "id": "8zjlTaUIqeACbBrh6FrY-Q2xIXsw5b5rDahvxqH36Ys"
     },
-    "https://bafybeidm3sremjulcdqefulerybnjqtzcf2o3vvyu5ayg35lbthmhxs5hi.ipfs.dweb.link/7.png": {
+    "images/seven.png": {
       "id": "znxrxVhRFK1vfAvMwTZxWp4-BL8zBD8SQd_GgtkvEfc"
     },
-    "https://bafybeidm3sremjulcdqefulerybnjqtzcf2o3vvyu5ayg35lbthmhxs5hi.ipfs.dweb.link/8.png": {
+    "images/eight.png": {
       "id": "-ebk0BQ_T8vKsAhj6nnamH4A7f9iX_cC76g_vS-xYiw"
     },
-    "https://bafybeidm3sremjulcdqefulerybnjqtzcf2o3vvyu5ayg35lbthmhxs5hi.ipfs.dweb.link/9.png": {
+    "images/nine.png": {
       "id": "E7X3tRPinAmWIZLLoLdxyHTzJY0X2Wi4D3PuVQnT4AM"
     },
-    "https://bafybeidm3sremjulcdqefulerybnjqtzcf2o3vvyu5ayg35lbthmhxs5hi.ipfs.dweb.link/10.png": {
+    "images/ten.png": {
       "id": "GMOiQptD0gTzhHkchcDvL8JO6oQBpmiSLdNLL1ujiTc"
     },
-    "https://bafybeidm3sremjulcdqefulerybnjqtzcf2o3vvyu5ayg35lbthmhxs5hi.ipfs.dweb.link/11.png": {
+    "images/eleven.png": {
       "id": "dRV_HK5fR-jmXEqXvSj6UB2z-eSI7xUvJbQWNjP_DaY"
     },
-    "https://bafybeidm3sremjulcdqefulerybnjqtzcf2o3vvyu5ayg35lbthmhxs5hi.ipfs.dweb.link/12.png": {
+    "images/twelve.png": {
       "id": "eofIhaehyRucDrWDQpVpoLFYEnQSWBnV6zJ_rwDMTHA"
     },
-    "https://bafybeidm3sremjulcdqefulerybnjqtzcf2o3vvyu5ayg35lbthmhxs5hi.ipfs.dweb.link/13.png": {
+    "images/thirteen.png": {
       "id": "5uAtB9giMOEM2m2UfxIdmKPYSy01hD-Z0UI_quY45z8"
     },
-    "https://bafybeidm3sremjulcdqefulerybnjqtzcf2o3vvyu5ayg35lbthmhxs5hi.ipfs.dweb.link/14.png": {
+    "images/fourteen.png": {
       "id": "0AtGGPZKgaVaPga65VltAlUY-40q6We_EpLx9QUzBhg"
     },
-    "https://bafybeidm3sremjulcdqefulerybnjqtzcf2o3vvyu5ayg35lbthmhxs5hi.ipfs.dweb.link/15.png": {
+    "images/fifteen.png": {
       "id": "-Sp_8fLvO7WewgOGQk2k-1Nupr0nB3MLnJnucTHXqfA"
     },
-    "https://bafybeidm3sremjulcdqefulerybnjqtzcf2o3vvyu5ayg35lbthmhxs5hi.ipfs.dweb.link/16.png": {
+    "images/sixteen.png": {
       "id": "LhDdy2vDuZG2_Ae0ejWnzcVk64qQOeu5pu97iAgB6GU"
     },
-    "https://bafybeidm3sremjulcdqefulerybnjqtzcf2o3vvyu5ayg35lbthmhxs5hi.ipfs.dweb.link/17.png": {
+    "images/seventeen.png": {
       "id": "MxGIRaYWgyr2xJnJiwGh4s9It1Ejid4GWvyyhmGbw30"
     },
-    "https://bafybeidm3sremjulcdqefulerybnjqtzcf2o3vvyu5ayg35lbthmhxs5hi.ipfs.dweb.link/18.png": {
+    "images/eighteen.png": {
       "id": "uU3ZDL5UiAcrbkFxMFhu6FoakmT6YwKjwGbEgJKP7BM"
     },
-    "https://bafybeidm3sremjulcdqefulerybnjqtzcf2o3vvyu5ayg35lbthmhxs5hi.ipfs.dweb.link/19.png": {
+    "images/nineteen.png": {
       "id": "RhVg5LCykU-fK2Lq8ErnRq0o1k8eCaF9nYlLvQZFYiI"
     },
-    "https://bafybeidm3sremjulcdqefulerybnjqtzcf2o3vvyu5ayg35lbthmhxs5hi.ipfs.dweb.link/20.png": {
+    "images/twenty4.png": {
       "id": "whK0Nhy72O-CCNRXgmUlwge1SYMHyhrV_6FAO6uvkaU"
     },
-    "https://bafybeidm3sremjulcdqefulerybnjqtzcf2o3vvyu5ayg35lbthmhxs5hi.ipfs.dweb.link/21.png": {
+    "images/twenty5.png": {
       "id": "V0XXw42td3rmbJ4i8ijMfK0Ho_-meeqC_HCJsTxUIuk"
-    },
-    "https://bafybeidm3sremjulcdqefulerybnjqtzcf2o3vvyu5ayg35lbthmhxs5hi.ipfs.dweb.link/22.png": {
-      "id": "jWDJ2L88pDjfG_FwVRVBV--RJU2Kk_K7lld-Yvrw36M"
     },
     "js/crudeboys_image.json": {
       "id": "hOi37GKaCGipB-8mD-psqDCkIAUGv1BC9AH_xgt8hP4"
